9f753100b5c89d3e67bbff6420d5e8a7805d8436
minhld <minhld@canifa.com>
2017-01-23 16:46:40 +0700

    bo sung truong author/email
    Summary: test
    
    Test Plan: test
    
    Reviewers: thonx
    
    Differential Revision: http://phabricator.canifa.com:8081/D39

--- custom_addons/website_vote/models/vote.py
+++ custom_addons/website_vote/models/vote.py
@@ -9,3 +9,9 @@ class Vote(models.Model):
     vote_count = fields.Integer('Vote Count')
     user_id = fields.Many2one('res.users', 'User')
     blog_post_id = fields.Many2one('blog.post', 'Blog Post')
+
+
+class BlogPost(models.Model):
+    _inherit = "blog.post"
+
+    author_email = fields.Char('Author/Email')
