8bd8a6e9ab8458963f3b12f418fdb88d3eb70dff
minhld <minhld@canifa.com>
2017-03-06 13:22:07 +0700

    update source
    Summary: test
    
    Test Plan: test
    
    Reviewers: thonx
    
    Differential Revision: http://phabricator.canifa.com:8081/D46

--- custom_addons/cnf_purchase_import_order_line/wizard/__init__.py
+++
@@ -1,3 +0,0 @@
-# -*- coding: utf-8 -*-
-# Copyright Canifa (<http://canifa.com>).
-import purchase_orderline_import_wizard
--- custom_addons/cnf_stock_request_detail_import/wizard/__init__.py
+++
@@ -1,3 +0,0 @@
-# -*- coding: utf-8 -*-
-# Copyright Canifa (<http://canifa.com>).
-import stock_request_detail_import
